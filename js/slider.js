38639044e5a9d372a09efb96a18a99a3e2b0c9ea
Taras Lobazov <zzadiraz@gmail.com>
2017-05-08 21:18:37 +0300

    Кое как шапка

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Admin on 07.05.2017.
+ */
+$(document).ready()
+$(window).load(function() {
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
+});
---
+++ slider.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Admin on 07.05.2017.
+ */
